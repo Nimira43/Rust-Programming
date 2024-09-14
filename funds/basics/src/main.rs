4261256b062283d6cbc6b26093f2344a738b642f
Nimira43 <n.rabone72@aol.com>
2024-09-14 14:10:10 +0100

    Scalar data types

--- funds/basics/src/main.rs
+++ funds/basics/src/main.rs
@@ -1,3 +1,24 @@
+// Scalar Data Types
+
+
 fn main() {
-    println!("Hello, world!");
+    let unsign: u8 = 5; // u16, u32
+    let signed: i8 = 5; // i16, i32
+    let float: f32 = 5.0;  
+
+    let arch_1: usize = 5;
+    let arch_: isize =  5;
+
+    let char: char = 'a'
+    let b: bool = true
+
+    // type aliasing
+    type Age = u8;
+    let bob_age: Age = 54;
+    // type conversion
+    let a: i32 = 10;
+    let b: f64 = a as f64;
+
+
 }
+
