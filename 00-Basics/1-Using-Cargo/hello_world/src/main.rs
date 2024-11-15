907988b8b3587852f0431a1b870c99c66e01b598
Nimira43 <n.rabone72@aol.com>
2024-11-15 05:20:51 +0000

    Cargo build

--- 00-Basics/1-Using-Cargo/hello_world/src/main.rs
+++ 00-Basics/1-Using-Cargo/hello_world/src/main.rs
@@ -8,6 +8,7 @@ to run                                      rustc main.rs
 to run exe                                  ./main
 formatting (like Prettier)                  rustfmt main.rs
 format all files                            cargo fmt
+compiles all code in directory              cargo build
 
 
 */
