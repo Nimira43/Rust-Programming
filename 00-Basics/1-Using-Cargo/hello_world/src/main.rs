8505333edc561fde7fa5cf5e94044a1a0d12147d
Nimira43 <n.rabone72@aol.com>
2024-11-27 04:20:15 +0000

    Small cheatsheet of Rust commands

--- 00-Basics/1-Using-Cargo/hello_world/src/main.rs
+++ 00-Basics/1-Using-Cargo/hello_world/src/main.rs
@@ -12,4 +12,37 @@ compiles all code in directory              cargo build
 compiles final release code                 cargo build --release
 deletes all files in target                 cargo clean
 
+compiles and runs                           cargo run
+
+Compile: rustc main.rs
+
+Run: cargo run
+
+Build: cargo build
+
+Clean: cargo clean
+
+Test: cargo test
+
+Check: cargo check
+
+Doc: cargo doc
+
+Cargo Commands:
+New Project: cargo new project_name
+
+Add Dependency: cargo add dependency_name
+
+Update Dependencies: cargo update
+
+List Dependencies: cargo tree
+
+Remove Dependency: cargo rm dependency_name
+
+Rust Compiler Options:
+Enable Linting: rustc -- -D warnings
+
+Optimize: rustc --release
+
+Target Specific Architecture: rustc --target <architecture>
 */
