ed1b1405c9d9a05104349c63a308ab3ce175236c
Nimira43 <n.rabone72@aol.com>
2024-11-27 04:09:17 +0000

    More on cargo build

--- 00-Basics/1-Using-Cargo/hello_world/src/main.rs
+++ 00-Basics/1-Using-Cargo/hello_world/src/main.rs
@@ -9,6 +9,7 @@ to run exe                                  ./main
 formatting (like Prettier)                  rustfmt main.rs
 format all files                            cargo fmt
 compiles all code in directory              cargo build
-
+compiles final release code                 cargo build --release
+deletes all files in target                 cargo clean
 
 */
