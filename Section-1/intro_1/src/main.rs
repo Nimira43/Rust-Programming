47ace03ccd77bcc36947effd8044bac9aacb648e
Nimira43 <n.rabone72@aol.com>
2025-09-25 20:47:37 +0100

    Interpolation

--- Section-1/intro_1/src/main.rs
+++ Section-1/intro_1/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    
+    let oranges = 4;
+    let apples = 5 + 6;
+    let fruits = oranges + apples;
+    println!("Bob has {} oranges.", oranges);
+    println!("There are {} fruits.", fruits);
 }
