2ef049b739640bb8dc405c7177e4fd0091b87a7a
Nimira43 <n.rabone72@aol.com>
2025-09-25 20:52:50 +0100

    Positional arguments

--- Section-1/intro_1/src/main.rs
+++ Section-1/intro_1/src/main.rs
@@ -4,4 +4,5 @@ fn main() {
     let fruits = oranges + apples;
     println!("Bob has {} oranges.", oranges);
     println!("There are {fruits} fruits.");
+    println!("There are {0} oranges and {1} apples.", oranges, apples);
 }
