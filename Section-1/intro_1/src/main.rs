cdcd7899109e1e53d31dd8fed490e11b4dec75cb
Nimira43 <n.rabone72@aol.com>
2025-09-25 20:39:22 +0100

    notes

--- Section-1/intro_1/src/main.rs
+++ Section-1/intro_1/src/main.rs
@@ -1,12 +1,3 @@
 fn main() {
-    // This is a comment
-    /*
-    Multi
-    Line
-    Comment
-    */
-
-    println!("Rust Programming");
-    println!("Introduction");
-    println!("Printing");
+    
 }
