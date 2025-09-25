40e884d24ace28c2f6e77ac56db0f1317d2ec1ed
Nimira43 <n.rabone72@aol.com>
2025-09-25 21:35:16 +0100

    Immutable and mutable variables. Also refining the overall project setup and workspace.

--- Section-1/intro_1/src/main.rs
+++ Section-1/intro_1/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
-    let work_hours = 8;
-    println!("I am going to work for {work_hours} today.");
+    let mut work_hours = 8;
+    println!("I am going to work for {work_hours} hours today.");
 
     work_hours = 11;
+
+    println!("I am going to work for {work_hours} hours tomorrow.");
 }
