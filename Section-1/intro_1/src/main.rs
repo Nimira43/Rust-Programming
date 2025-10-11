90e7825cd7eed0ccba4fb49a5f4dd5c4670b6b1a
Nimira43 <n.rabone72@aol.com>
2025-10-11 02:19:32 +0100

    Second example

--- Section-1/intro_1/src/main.rs
+++ Section-1/intro_1/src/main.rs
@@ -3,4 +3,6 @@ fn main() {
     println!("{}", grams_of_flour);
     let grams_of_flour = 84.57; // variable shadowing
     println!("{}", grams_of_flour);
+    let grams_of_flour = 84; // variable shadowing
+    println!("{}", grams_of_flour);
 }
