8c75d13ca4f00c633d84c1ab560d1c221b77a5de
Nimira43 <n.rabone72@aol.com>
2025-10-11 02:07:21 +0100

    Archiving example

--- Section-1/intro_1/src/main.rs
+++ Section-1/intro_1/src/main.rs
@@ -1,8 +1,3 @@
 fn main() {
-    let mut work_hours = 8;
-    println!("I am going to work for {work_hours} hours today.");
-
-    work_hours = 11;
-
-    println!("I am going to work for {work_hours} hours tomorrow.");
+    
 }
