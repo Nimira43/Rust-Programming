c77fe36aa95f31d20d0c09e23636f1e5b0c40fd3
Nimira43 <n.rabone72@aol.com>
2025-10-25 02:04:44 +0100

    Soft reboot

--- Section-1/intro_1/src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let grams_of_flour = "84.57";
-    println!("{}", grams_of_flour);
-    let grams_of_flour = 84.57; // variable shadowing
-    println!("{}", grams_of_flour);
-    let grams_of_flour = 84; // variable shadowing
-    println!("{}", grams_of_flour);
-}
