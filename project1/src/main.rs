3418a4e5dc59e2ab14355fe5a843f0a81e6f3134
Nimira43 <n.rabone72@aol.com>
2025-10-25 09:34:54 +0100

    Comments

--- project1/src/main.rs
+++ project1/src/main.rs
@@ -1,8 +1,20 @@
 use std::io;
 
+/// Crate comment
+/// This is used to document a Rust program
+
 fn main() {
+    // A simple Rust program
+
     let mut input = String::new();
     println!("Hello there!");
+    
+    /*
+    multi 
+    line
+    comment
+    */
+
     match io::stdin().read_line(&mut input) {
         Ok(_) => {
             println!("How is {}", input.trim());
