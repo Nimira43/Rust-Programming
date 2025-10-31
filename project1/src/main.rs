64caa3fa5087e0f54978a550340020a4eb83d6cd
Nimira43 <n.rabone72@aol.com>
2025-10-31 22:53:00 +0000

    SliceRandom and thread_rng

--- project1/src/main.rs
+++ project1/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
+    
+
     let mut countries = vec!["Albania", "Andorra", "Armenia", "Austria", "Azerbaijan", "Belarus", "Belgium", "Bosnia", "Bulgaria", "Croatia", "Cyprus", "Czechia", "Denmark", "England", "Estonia", "Faroe Islands", "Finland", "France", "Georgia", "Germany", "Gibraltar", "Greece", "Hungary", "Iceland", "Israel", "Italy", "Kazakhstan", "Kosovo", "Latvia", "Liechtenstein", "Lithuania", "Luxembourg", "Malta", "Moldova", "Montenegro", "Netherlands", "North Macedonia", "Northern Ireland", "Norway", "Poland", "Portugal", "Republic of Ireland", "Romania", "Russia", "San Marino", "Scotland", "Serbia", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Türkiye", "Ukraine", "Wales"];
+    
+    use rand::seq::SliceRandom;
+    use rand::thread_rng;
 }
