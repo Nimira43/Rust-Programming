460d6573d8f77c80ed356c1263108e4d50fc4055
Nimira43 <n.rabone72@aol.com>
2025-10-31 21:53:31 +0000

    Positional arguments

--- project1/src/main.rs
+++ project1/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     println!("I am a {} named {}.", "cat", "Lenny");
     println!("a + b = {}", 8+6);
+    println!("There is a {2} with {4} {0} and {1} {3}.", "apples", 14, "basket","oranges", 8);
+    println!("There is a {2} with {4} {0} and {1} {3}.", "peaches", 5, "bag","pears", 7);
 }
 
 
