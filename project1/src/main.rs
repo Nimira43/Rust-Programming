e80b48bf0aef314212d5314dd344dda66be7c827
Nimira43 <n.rabone72@aol.com>
2025-10-31 21:37:24 +0000

    archived current code

--- project1/src/main.rs
+++ project1/src/main.rs
@@ -1,37 +1,3 @@
-use std::io;
-
-/// Crate comment
-/// This is used to document a Rust program
-
-fn main() {
-    
-    //! # Input Program
-    //! 
-    //! ```rust
-    //! fn main()
-    //! ```
-    //! 
-    //! Reads user input and then prints it to the console
 
 
-    let mut input = String::new();
-    println!("Hello there!");
-    
-    /*
-    multi 
-    line
-    comment
-    */
-    
-    // A single line comment
-    
-    match io::stdin().read_line(&mut input) {
-        Ok(_) => {
-            println!("How is {}", input.trim());
-        },
-        Err(e) => {
-            println!("Something went wrong! {}", e);
-        }
-    }
-}
 
