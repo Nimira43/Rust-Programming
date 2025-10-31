bb87ab7dd64c7d42717dfe9d2fe56a357859749a
Nimira43 <n.rabone72@aol.com>
2025-10-31 23:04:21 +0000

    Looping through groups, identifying each one with a number, and printing the results/

--- project1/src/main.rs
+++ project1/src/main.rs
@@ -1,11 +1,9 @@
-use std::os::windows::thread;
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 
 fn main() {
     let mut countries = vec!["Albania", "Andorra", "Armenia", "Austria", "Azerbaijan", "Belarus", "Belgium", "Bosnia", "Bulgaria", "Croatia", "Cyprus", "Czechia", "Denmark", "England", "Estonia", "Faroe Islands", "Finland", "France", "Georgia", "Germany", "Gibraltar", "Greece", "Hungary", "Iceland", "Israel", "Italy", "Kazakhstan", "Kosovo", "Latvia", "Liechtenstein", "Lithuania", "Luxembourg", "Malta", "Moldova", "Montenegro", "Netherlands", "North Macedonia", "Northern Ireland", "Norway", "Poland", "Portugal", "Republic of Ireland", "Romania", "Russia", "San Marino", "Scotland", "Serbia", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Türkiye", "Ukraine", "Wales"];
     
-    use rand::seq::SliceRandom;
-    use rand::thread_rng;
-
     let mut rng = thread_rng();
     countries.shuffle(&mut rng);
 
@@ -13,4 +11,12 @@ fn main() {
         .chunks(5)
         .map(|chunk| chunk.to_vec())
         .collect();
+
+    for (i, group) in groups.iter().enumerate() {
+        println!("Group {}:", i + 1);
+        for team in group {
+            println!(" - {}", team);
+        }
+        println!();
+    }
 }
