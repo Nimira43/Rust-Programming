b9d60a4df0338f3e93e7bf70efde130259052e03
Nimira43 <n.rabone72@aol.com>
2025-10-31 23:23:23 +0000

    Randomising each group to create final positions

--- project1/src/main.rs
+++ project1/src/main.rs
@@ -2,8 +2,12 @@ use rand::seq::SliceRandom;
 use rand::thread_rng;
 
 fn main() {
+    // UEFA countries data
+
     let mut countries = vec!["Albania", "Andorra", "Armenia", "Austria", "Azerbaijan", "Belarus", "Belgium", "Bosnia", "Bulgaria", "Croatia", "Cyprus", "Czechia", "Denmark", "England", "Estonia", "Faroe Islands", "Finland", "France", "Georgia", "Germany", "Gibraltar", "Greece", "Hungary", "Iceland", "Israel", "Italy", "Kazakhstan", "Kosovo", "Latvia", "Liechtenstein", "Lithuania", "Luxembourg", "Malta", "Moldova", "Montenegro", "Netherlands", "North Macedonia", "Northern Ireland", "Norway", "Poland", "Portugal", "Republic of Ireland", "Romania", "Russia", "San Marino", "Scotland", "Serbia", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Türkiye", "Ukraine", "Wales"];
     
+    // Phase 1 Draw 11 groups of 5
+
     let mut rng = thread_rng();
     countries.shuffle(&mut rng);
 
@@ -19,4 +23,11 @@ fn main() {
         }
         println!();
     }
+
+    // Phase 1 - Final Standings
+
+    for group in groups.iter_mut() {
+        group.shuffle(&mut rng);
+    }
+
 }
