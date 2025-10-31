69a80c062d7851b1aaee22234f034b0af62d9a29
Nimira43 <n.rabone72@aol.com>
2025-10-31 22:33:30 +0000

    Fill countries array with all UEFA nations

--- project1/src/main.rs
+++ project1/src/main.rs
@@ -1,9 +1,4 @@
 fn main() {
-    countries = []
+    countries = ["Albania", "Andorra", "Armenia", "Austria", "Azerbaijan", "Belarus", "Belgium", "Bosnia", "Bulgaria", "Croatia", "Cyprus", "Czechia", "Denmark", "England", "Estonia", "Faroe Islands", "Finland", "France", "Georgia", "Germany", "Gibraltar", "Greece", "Hungary", "Iceland", "Israel", "Italy", "Kazakhstan", "Kosovo", "Latvia", "Liechtenstein", "Lithuania", "Luxembourg", "Malta", "Moldova", "Montenegro", "Netherlands", "North Macedonia", "Northern Ireland", "Norway", "Poland", "Portugal", "Republic of Ireland", "Romania", "Russia", "San Marino", "Scotland", "Serbia", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Türkiye", "Ukraine", "Wales"];
+
 }
-
-
-
- 
-
-
