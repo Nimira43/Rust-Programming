94f1eeec9fa9404a38ad084f6865af705aacd4af
Nimira43 <n.rabone72@aol.com>
2025-11-18 00:48:36 +0000

    Variables - 1

--- project2/src/main.rs
+++ project2/src/main.rs
@@ -1,7 +1,17 @@
+#[allow(unused_variables)]
+
 fn main() {
 
     // Traits
     println!("binary: {:b}, hex: {:x}, octal: {:o}", 30, 30, 30);
     // Debug
-    println!("Array {:?}", [1, 2, 3]);
+    println!("Array: {:?}", [1, 2, 3]);
+
+    // Variables
+    let name = "Bob";
+    let age = 53;
+    let amount1 = 2147483647;
+    let amount2 = 2147483648;
+
+
 }
