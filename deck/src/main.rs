8e394a481d4590202218bb2646a096324b479be0
Nimira43 <n.rabone72@aol.com>
2025-11-20 13:47:18 +0000

    deal function - 2

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -8,14 +8,14 @@ struct Deck {
 
 impl Deck {
     fn new() -> Self {
-        let suits = vec!["Spades", "Diamonds", "Hearts", "Clubs"]
-        let values = vec!["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+        let suits = vec!["Spades", "Diamonds", "Hearts", "Clubs"];
+        let values = vec!["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
 
-        let mut cards = Vec::new()
+        let mut cards = Vec::new();
 
         for suit in &suits {
             for value in &values {
-                cards.push(format("{} of {}", value, suit))
+                cards.push(format("{} of {}", value, suit));
             }
         }
 
@@ -24,9 +24,14 @@ impl Deck {
 
     fn print (&self) {
         for (i, card) in self.cards.iter().enumerate() {
-            println!("{}: {}", i, card)
+            println!("{}: {}", i, card);
         }  
     }
 
-    fn deal(&mut self, hand_size: usize) -> (Vec<String>0, Vec<String>) {}
+    fn deal(&mut self, hand_size: usize) -> (Vec<String>0, Vec<String>) {
+        let hand : Vec<String> = self.cards.drain(0..hand_size.min(self.cards.len())).collect();
+        (hand.clone(), self.cards.clone())
+    }
+
+    
 }
