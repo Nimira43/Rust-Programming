c477af8350aa9b5fa6caa0ee2f599be4d4715474
Nimira43 <n.rabone72@aol.com>
2025-11-20 13:14:30 +0000

    Deck - 3

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -10,5 +10,13 @@ impl Deck {
     fn new() -> Self {
         let suits = vec!["Spades", "Diamonds", "Hearts", "Clubs"]
         let values = vec!["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+
+        let mut cards = Vec::new()
+
+        for suit in &suits {
+            for value in &values {
+                cards.push(format("{} of {}", value, suit))
+            }
+        }
     }
 }
